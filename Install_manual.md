2a111189c6a0d0781a7c2c86f7ac8914caa012a8
Mike Robinson <mrobinson@nortoncottage.com>
2024-03-27 10:33:18 -0500

    Tighten up and remove expected username and home path

--- Install_manual.md
+++ Install_manual.md
@@ -1,7 +1,10 @@
-# Al the things a new Mac needs
+# All the things a new Mac needs
+
+* Config Apple account, ensure iCloud done.
 
 * run setup_new_mac.sh
 * install my comics mono font (https://www.myfonts.com/account?view=order-history)
+
 * setup mail.app accounts in settings / Internet Accounts
 	* sixty4k@gmail.com
 	* sixty4k@fastmail.fm (bootyhouse.org and nortoncottage.com)
@@ -11,3 +14,18 @@
 	* add it to local_exports.conf (export GHAPI_KEY=<key material>)
 
 * clone all GH repos (`bin/clone_gh_repos.sh`)
+
+* install driver for Scarlett 6i6 USB:
+	https://downloads.focusrite.com/focusrite/scarlett-1st-gen/scarlett-6i6-1st-gen
+
+* Setup Loopback
+	* open app to install ACE engine
+	* /Library/Application\ Support/Loopback/
+	* figure out if I can save the devices plist
+		* `plutil -convert xml1 Devices.plist` to make it a xml not a binary
+		* `plutil -convert binary1 Devices.plist` to make it binary again
+
+* OBS: Old setup is crufty, need to make a clean new one and save it out.
+
+* OBS Plugins
+	*
--- bin/setup_new_mac.sh
+++ bin/setup_new_mac.sh
@@ -7,16 +7,28 @@ hitenter() {
 
 cd ~
 
+if [ -d ${HOME}/Library/Mobile\ Documents/com~apple~CloudDocs ]; then
+
+  echo "> ssh key setup - needs iCLoud Setup"
+  rsync -avz ${HOME}/Library/Mobile\ Documents/com~apple~CloudDocs/.ssh .
+  chmod 600 ~/.ssh/*
+
+  echo "> Comic Code Font"
+  cp ${HOME}/Library/Mobile\ Documents/com~apple~CloudDocs/new_system/ComicCode* ${HOME}/Library/Fonts/
+
+else
+  echo "! No iCloud setup - not going to be able to run this script"
+  exit 99
+fi
+
+echo "> Create ~/Repos"
 mkdir Repos
-
-rsync -avz /Users/mrobinson/Library/Mobile\ Documents/com~apple~CloudDocs/.ssh .
-chmod 600 ~/.ssh/*
-
 cd Repos
 git clone git@github.com:sixty4k/profile.git
 
 cd profile/bin
 
+echo "> Run Setup scripts from profile repo"
 ./defaults.sh
 ./setup_xcode_cli.sh
 ./homebrew.sh
@@ -27,27 +39,27 @@ echo "> 1Password"
 open "/Applications/1Password 7.app"
 hitenter 1Password
 
-echo "Setup Alfred"
+echo "> Alfred"
 echo "License key in 1Password"
 open "$(ls -d /Applications/Alfred*)"
 hitenter Alfred
 
-echo "Setup iTerm"
+echo "> iTerm"
 echo "iCloud setting file is in iCloud Drive/Documents/AppSupport/iTerm"
 echo "git saved version is in Repos/profile/configs/iTerm"
 open "/Applications/iTerm.app"
 hitenter iTerm
 
-echo "Setup Sublime Text"
+echo "> Sublime Text"
 echo "Start App, input license from 1Pass, then setup saved config"
 echo "Apparently my ST config is super fubar." 
 open "/Applications/Sublime Text.app"
 hitenter Sublime_Text
 
-echo "Setup Amethyst"
+echo "> Amethyst"
 open "/Applications/Amethyst.app"
 hitenter Amethyst
 
-echo "Setup Bash Profile"
+echo "> Bash Profile"
 curl -s https://raw.githubusercontent.com/sixty4k/profile/master/install | bash
 
